a9a48d2e904a5f9d37e1d7e92f6450b9d729364a
mosaadaldeen <mohammadsd97@gmail.com>
2021-06-14 15:14:09 +0200

    initial setup, first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Document</title>
+  </head>
+  <body>
+
+  <form action="search.php" method="get" class="col-md-4 offset-md-4 mt-5 pt-3">
+      <div class="input-group mb-3">
+          <input type="text" name="search"  id="search"  class="form-control" placeholder="Search for hotels ......">
+          <div class="input-group-append">
+          </div>
+        </div>
+    </form>
+
+  <div id="result"></div>
+
+    <?php
+      $json = json_decode(file_get_contents('hotels.json'), true);
+
+                foreach ($json as $key => $value) {
+                  echo "
+                  <ul style='width: 300px;'' class='list-group mx-auto'>
+                    <li class='list-group-item text-white bg-dark'>{$value['code']}</li>
+                    <li class='list-group-item' data-userid='{$value['code']}' style='cursor:pointer;'>{$value['hotel']}</li>
+                    <li class='list-group-item'>{$value['postCode']}</li>
+                    <li class='list-group-item'>{$value['straat']}</li>
+                    <li class='list-group-item'>{$value['regio']}</li>
+                    <li class='list-group-item'>{$value['provinsie']}</li>
+                    <li class='list-group-item'>{$value['nummer']}</li>
+                    <li class='list-group-item'>{$value['email']}</li>
+                  </ul>
+                  <br />";
+              }
+
+      ?>
+  
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script type="text/javascript" src="./main.js"></script>
+</body>
+</html>
---
+++ main.js
@@ -0,0 +1,60 @@
+$(document).ready(function () {
+  let hotelName = $("ul li:nth-child(2)");
+
+  $("#search").keyup(function () {
+    var value = $(this).val().toLowerCase();
+    hotelName.filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+      $("#result").html(hotelName);
+
+      hotelName.click(function (event) {
+        let userId = event.target.dataset.userid;
+        console.log(userId);
+        window.location.href = "./search.php/" + userId;
+        return false;
+      });
+    });
+  });
+});
+
+// $(".form").submit(function (e) {
+//   e.preventDefault();
+//   function sendEmail() {
+//     var name = $("#name");
+//     var email = $("#email");
+//     var subject = $("#subject");
+//     var body = $("#body");
+
+//     if (
+//       isNotEmpty(name) &&
+//       isNotEmpty(email) &&
+//       isNotEmpty(subject) &&
+//       isNotEmpty(body)
+//     ) {
+//       $.ajax({
+//         url: "sendEmail.php",
+//         method: "POST",
+//         dataType: "json",
+//         data: {
+//           name: name.val(),
+//           email: email.val(),
+//           subject: subject.val(),
+//           body: body.val(),
+//         },
+//         success: function (response) {
+//           $("#myForm")[0].reset();
+//           $(".sent-notification").text("Message Sent Successfully.");
+//         },
+//       });
+//     }
+//   }
+
+//   function isNotEmpty(string) {
+//     if (string.val() == "") {
+//       string.css("border", "1px solid red");
+//       return false;
+//     } else string.css("border", "");
+//     return true;
+//   }
+//   sendEmail();
+// });
